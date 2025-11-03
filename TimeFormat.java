cc3c5adb06ff7f3f9b23a682e80a2eff4e70cf40
Lear Eldar <lear.eldar@post.runi.ac.il>
2025-11-03 15:24:53 +0200

    Made the code for TimeFormat

--- TimeFormat.java
+++ TimeFormat.java
@@ -21,14 +21,15 @@ public class TimeFormat {
 		} else {
 			minuString = "" + minutes;
 		}
-		
 
-	if (hours > 12) {
-		hours = hours - 12;
-		System.out.println(hours + ":" + minuString + " PM");
-	} else {
-		System.out.println(hours + ":" + minuString + " AM" );
-	}
+		if (hours > 12) {
+			System.out.print(hours + ":" + minuString + " PM");
+		} else if (hours == 12) {
+			System.out.print(hours + ":" + minuString + " PM");
+		} else {
+			System.out.println(hours + ":" + minuString + " AM" )
+		}
+		
 	
 	
 	}
